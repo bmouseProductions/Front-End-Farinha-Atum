fe081db8b12fb0ce5dffca4dd6cff620f44cd33e
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-07-14 15:06:50 -0300

    alert email enviado

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,33 +1,46 @@
-import axios from "axios"
+import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://backend-farinhas-patense.onrender.com"
-})
+  baseURL: "https://backend-farinhas-patense.onrender.com",
+});
 
 interface propsFormData {
-    nome: string;
-    telefone: string;
-    email: string;
-    mensagem: string;
-    propostaFile: File | null;
-    propostaName: string;
+  nome: string;
+  telefone: string;
+  email: string;
+  mensagem: string;
+  propostaFile: File | null;
+  propostaName: string;
 }
 
-export const enviarEmail = async (formData:propsFormData) => {
-    const {nome, telefone, email, mensagem, propostaFile, propostaName} = formData
+export const enviarEmail = async (formData: propsFormData) => {
+  const { nome, telefone, email, mensagem, propostaFile, propostaName } = formData;
 
-    const formDataToSend = new FormData();
-    formDataToSend.append("nome", nome);
-    formDataToSend.append("telefone", telefone);
-    formDataToSend.append("email", email);
-    formDataToSend.append("mensagem", mensagem);
+  const formDataToSend = new FormData();
+  formDataToSend.append("nome", nome);
+  formDataToSend.append("telefone", telefone);
+  formDataToSend.append("email", email);
+  formDataToSend.append("mensagem", mensagem);
 
-    if(propostaFile){
-        formDataToSend.append("propostaFile", propostaFile, propostaName)
+  if (propostaFile) {
+    formDataToSend.append("propostaFile", propostaFile, propostaName);
+  }
+
+  try {
+    const response = await api.post("/send", formDataToSend, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    if (response.status === 200) {
+      window.alert("Email enviado com sucesso!");
+    } else {
+      window.alert("Falha ao enviar o email. Por favor, tente novamente.");
     }
 
-    return await api.post("/send", formDataToSend, {
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-
-}
+    return response;
+  } catch (error) {
+    console.error("Erro ao enviar o email:", error);
+    window.alert("Falha ao enviar o email. Por favor, tente novamente.");
+    throw error;
+  }
+};
